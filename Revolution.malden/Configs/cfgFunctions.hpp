41d16b4ac31c49359ea13fe9b926b0b327ca4a3a
Koalas <46250330+koalas11@users.noreply.github.com>
2019-12-28 15:14:41 +0100

    Update cfgFunctions.hpp

--- Revolution.malden/Configs/cfgFunctions.hpp
+++ Revolution.malden/Configs/cfgFunctions.hpp
@@ -1,34 +1,42 @@
 class CfgFunctions 
 {
-	class REV 
-	{
-		class DatabaseScripts
-		{
-			class initServerDatabase {};
-			class initPlayerDatabase {};
-			class savePlayerData {};
-			class startSendingDynamicData {};
-			class getDatabaseFile {};
-		};
+    class REV 
+    {
+        class DatabaseScripts
+        {
+            class initServerDatabase {};
+            class initPlayerDatabase {};
+            class savePlayerData {};
+            class startSendingDynamicData {};
+            class getDatabaseFile {};
+        };
 
-		class FinancialScripts 
-		{
-			class formatMoney {};
-			class openBankGUI {};
-			class openSmallBankGUI {};
-			class transferMoneyServer {};
-			class addMoneyServer {};
-			class initAllATMs {};
-		};
+        class FinancialScripts 
+        {
+            class formatMoney {};
+            class openBankGUI {};
+            class openSmallBankGUI {};
+            class transferMoneyServer {};
+            class addMoneyServer {};
+            class initAllATMs {};
+        };
+        
+        class Farming
+        {
+            class GatherBody {};
+            class GatherResources {};
+        };
 
-		class InteractionScripts 
-		{
-			class addInteraction {};
-		};
+        class InteractionScripts 
+        {
+            class addInteraction {};
+            class InteractionKey {};
+        };
 
-		class UtilityScripts 
-		{
-			class error {};
-		};
-	};
+        class UtilityScripts 
+        {
+            class error {};
+            class KeyHandler {};
+        };
+    };
 };
