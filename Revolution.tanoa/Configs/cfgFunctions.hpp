d966d759374d1a56894e91511717d630f423d774
Koalas <46250330+koalas11@users.noreply.github.com>
2020-01-23 16:13:07 +0100

    Update cfgFunctions.hpp

--- Revolution.tanoa/Configs/cfgFunctions.hpp
+++ Revolution.tanoa/Configs/cfgFunctions.hpp
@@ -1,34 +1,55 @@
 class CfgFunctions 
 {
-	class REV 
-	{
-		class DatabaseScripts
-		{
-			class initServerDatabase {};
-			class initPlayerDatabase {};
-			class savePlayerData {};
-			class startSendingDynamicData {};
-			class getDatabaseFile {};
-		};
+    class REV 
+    {
+        class DatabaseScripts
+        {
+            class initServerDatabase {};
+            class initPlayerDatabase {};
+            class savePlayerData {};
+            class startSendingDynamicData {};
+            class getDatabaseFile {};
+        };
 
-		class FinancialScripts 
-		{
-			class formatMoney {};
-			class openBankGUI {};
-			class openSmallBankGUI {};
-			class transferMoneyServer {};
-			class addMoneyServer {};
-			class initAllATMs {};
-		};
+        class FinancialScripts 
+        {
+            class formatMoney {};
+            class openBankGUI {};
+            class openSmallBankGUI {};
+            class transferMoneyServer {};
+            class addMoneyServer {};
+            class initAllATMs {};
+        };
+        
+        class FarmingScripts
+        {
+            class GatherBody {};
+            class GatherResources {};
+            class AnimalsSpawn {};
+            class ResourcesSpawn {};
+        };
 
-		class InteractionScripts 
-		{
-			class addInteraction {};
-		};
+        class InteractionScripts 
+        {
+            class addInteraction {};
+            class InteractionKey {};
+        };
 
-		class UtilityScripts 
-		{
-			class error {};
-		};
-	};
+        class UtilityScripts 
+        {
+            class error {};
+            class KeyHandler {};
+        };
+        
+        class VirtualItemsScripts 
+        {
+            class CraftingGUI {};
+            class CraftingHandler {};
+            class CraftingMaterialNeeded {};
+            class SlotsHandler {};
+            class VItemsHandlerPlayer {};
+            class VirtualItemsSetup {};
+            class VirtualItemsCheck {};
+        };
+    };
 };
