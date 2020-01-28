dd22b6252e034b2ad3e27c8bfab6a0a322317ba9
Koalas <46250330+koalas11@users.noreply.github.com>
2020-01-28 15:55:05 +0100

    Update ServerFunctions.hpp

--- Revolution.tanoa/Configs/ServerFunctions.hpp
+++ Revolution.tanoa/Configs/ServerFunctions.hpp
@@ -1,14 +1,21 @@
 class DatabaseScriptsServer
 {
-	file = "Functions\Server\DatabaseScripts";
-	class initServerDatabase {};
-	class startSendingDynamicData {};
-	class getDatabaseFile {};
+    file = "Functions\Server\DatabaseScripts";
+    class initServerDatabase {};
+    class startSendingDynamicData {};
+    class getDatabaseFile {};
 };
 
 class FinancialScriptsServer
 {
-	file = "Functions\Server\FinancialScripts";
-	class transferMoneyServer {};
-	class addMoneyServer {};
+    file = "Functions\Server\FinancialScripts";
+    class transferMoneyServer {};
+    class addMoneyServer {};
+};
+
+class FarmingScriptsServer
+{
+    file = "Functions\Server\FarmingScripts";
+    class AnimalsSpawn {};
+    class ResourcesSpawn {};
 };
