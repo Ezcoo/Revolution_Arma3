aa3ff3254a8ab9b9913bd4f3cd0b15ac46c9989f
Koalas <46250330+koalas11@users.noreply.github.com>
2020-01-28 15:53:08 +0100

    Update ClientFunctions.hpp

--- Revolution.tanoa/Configs/ClientFunctions.hpp
+++ Revolution.tanoa/Configs/ClientFunctions.hpp
@@ -1,26 +1,47 @@
 class DatabaseScriptsClient
 {
-	file = "Functions\Client\DatabaseScripts";
-	class initPlayerDatabase {};
-	class savePlayerData {};
+    file = "Functions\Client\DatabaseScripts";
+    class initPlayerDatabase {};
+    class savePlayerData {};
 };
 
 class FinancialScriptsClient
 {
-	file = "Functions\Client\FinancialScripts";
-	class openBankGUI {};
-	class openSmallBankGUI {};
-	class initAllATMs {};
+    file = "Functions\Client\FinancialScripts";
+    class openBankGUI {};
+    class openSmallBankGUI {};
+    class initAllATMs {};
 };
 
 class InteractionScriptsClient
 {
-	file = "Functions\Client\InteractionScripts";
-	class addInteraction {};
+    file = "Functions\Client\InteractionScripts";
+    class addInteraction {};
+    class InteractionKey {};
 };
 
 class MiscellanousScriptsClient
 {
     file = "Functions\Client\MiscellanousScripts";
     class monitorSurroundings {};
+};
+
+class FarmingScriptsClient
+{
+    file = "Functions\Client\FarmingScripts";
+    class GatherBody {};
+    class GatherResources {};
+};
+
+class VirtualItemsScripts
+{
+    file = "Functions\Client\VirtualItemsScripts";
+    class CraftingAmountSet {};
+    class CraftingGUI {};
+    class CraftingHandler {};
+    class CraftingMaterialNeeded {};
+    class SlotsHandler{};
+    class VirtualItemsCheck {};
+    class VirtualItemsSetup {};
+    class VItemsHandlerPlayer {};
 };
